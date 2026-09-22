650d6cefc92138d3c69a7bb7ff7eb5feb3f3ce42
ㅤ <107886961+ErionNezha@users.noreply.github.com>
2026-09-22 23:57:40 +0200

    Sanitize database credentials

--- javaconnect.java
+++ javaconnect.java
@@ -2,14 +2,22 @@ import java.sql.*;
 import java.sql.DriverManager;
 
 
+
+
 public class javaconnect {
     Connection conn;
     public static java.sql.Connection ConnecrDb(){
         try{
 
-            java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3000/isp", "root", "pradumya");
+
+            // TODO: configure your own database credentials here
+            String url = "jdbc:mysql://localhost:3306/isp";
+            String user = "YOUR_DB_USER";
+            String password = "YOUR_DB_PASSWORD";
+            java.sql.Connection conn = DriverManager.getConnection(url, user, password);
             return conn;
         } catch(Exception e){
+
 
         }
         return null;
